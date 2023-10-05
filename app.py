04d7f47e43758204fc867940143294830593814a
Hashtagich <antonchikz@mail.ru>
2023-10-05 19:13:24 +0300

    Закончил проект. Дописал класс CriptoConvector, протестировал парсинг на все комбинации валют и отлов исключений на ввод, написал документацию.

--- app.py
+++ app.py
@@ -1,46 +1,54 @@
 import telebot
+from notifiers import get_notifier
 
-from config import *
 from extensions import *
 
 bot = telebot.TeleBot(TOKEN)
+telegram = get_notifier('telegram')
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
+def send_welcome(message: telebot.types.Message) -> None:
+    """Функция для отправки ответным сообщением информации о том как пользоваться ботом
+    при вводе команды /start или /help. Также отправляется сообщение разработчику о том, что бота начали использовать,
+    в целях контроля, что нет злоупотребления API запросами т.к. они ограничены (1000 в месяц)."""
     bot.reply_to(message, text=text_info)
+
+    message_to_me = f"Меня активировали. {CriptoConvector.get_info(message)}"
+    telegram.notify(token=TOKEN, chat_id=CREATOR_ID, message=message_to_me)  # функция, чтобы отслеживать кто исп-л бота
 
 
 @bot.message_handler(commands=['values'])
-def values_info(message: telebot.types.Message):
+def values_info(message: telebot.types.Message) -> None:
+    """Функция для вывода списка валют при вводе команды /values"""
     text = ('Список валют:',)
-    res = '\n✦ '.join(text + tuple(db_values.keys()))
+    res = '\n✦ '.join(text + tuple(db_values))
 
     bot.reply_to(message, text=res)
 
 
 @bot.message_handler(content_types=['text', ])
-def convert(message: telebot.types.Message):
+def convert(message: telebot.types.Message) -> None:
+    """Функция для активации конвертирования валюты (статический метод get_price класса CriptoConvector)
+    в соответствии с введённым текстом, если будет ошибка ввода, то будет возбуждение исключений и вывод сообщения
+    об конкретной ошибке. В случаи корректного ввода будет выведено сообщение с результатом конвертирования."""
     values = message.text.split(' ')
     try:
         if len(values) != 3:
-            raise ConversionException('Вы ввели количество параметров неравное 3.')
+            raise APIException('Вы ввели количество параметров неравное 3.')
 
         base, quote, amount = values
         sum_values = CriptoConvector.get_price(base, quote, amount)
 
-        res = f'Вы ввели первая валюта = {base} вторая валюта = {quote} число = {amount}'  # это потом удалить
-
-    except ConversionException as e:
+    except APIException as e:
         bot.reply_to(message, f'Ошибка пользователя.\n{e}')
 
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
 
     else:
-        text = f'Цена {db_values[base]} {db_values[quote]} {amount} - {sum_values}'
-        bot.send_message(message.chat.id, res)  # это потом удалить
-        bot.send_message(message.chat.id, text)  # это финальный вывод !!!верный!!!
+        text = f'Цена {amount} {db_values[base]} в {db_values[quote]} составляет {sum_values} {db_values[quote]}'
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling(none_stop=True)
--- extensions.py
+++ extensions.py
@@ -1,11 +1,61 @@
-class ConversionException(Exception):
+import requests
+import json
+
+from config import *
+
+
+class APIException(Exception):
     pass
 
 
 class CriptoConvector:
-    """Дока"""
+    """Класс для конвертирования стоимости валюты из одной в другую и проверки корректности введённых данных."""
 
     @staticmethod
-    def get_price(base: str, quote: str, amount: str) -> str:
-        result = base + quote + amount
-        return result
+    def get_price(base: str, quote: str, amount: str) -> float:
+        """Статический метод для конвертирования валюты из одной в другую.
+        1) Проверяется корректность ввода(передачи) данных при некорректности возбуждается исключение APIException;
+        2) После проверки данных происходит парсинг, чтобы получить коэффициент, если кол-во запросов будет больше 1000
+         за месяц, то возбуждается исключение Exception;
+        3) Функция возвращает результат конвертирования валюты в формате числа float."""
+
+        if base not in db_values.keys() and quote not in db_values.keys():
+            raise APIException(f'{base} и {quote} нет в списке валют.\n{text_values_command}')
+
+        elif base not in db_values.keys():
+            raise APIException(f'{base} нет в списке валют.\n{text_values_command}')
+
+        elif quote not in db_values.keys():
+            raise APIException(f'{quote} нет в списке валют.\n{text_values_command}')
+
+        elif base in db_values.keys() and quote in db_values.keys() and base == quote:
+            raise APIException(f'{base} и {quote} одинаковые валюты, результат будет {amount} {db_values[base]}. '
+                               f'Конвертирование не имеет смысла.')
+
+        try:
+            new_amount = amount.replace(',', '.')
+            new_amount = float(new_amount)
+        except ValueError:
+            raise APIException(f'При вводе {amount} допущена ошибка')
+
+        try:
+            base_en, quote_en = db_values[base], db_values[quote]
+            url = f'http://apilayer.net/api/live?access_key={API_CURRENCYLAYER}&currencies={quote_en}&source={base_en}&format=1'
+            request = requests.get(url=url)
+            db = json.loads(request.content)
+            course = db['quotes'][base_en + quote_en]
+
+        except Exception:
+            print('Прошу простить, но сервер с запросами курсов валют временно не доступен.')
+
+        else:
+            return new_amount * course
+
+    @staticmethod
+    def get_info(message) -> str:
+        """Статический метод возвращает строку определённого формата с содержанием информации о написавшем."""
+        return f"""\nВот информация: 
+        ID: {message.from_user.id};
+        FIRST_NAME: {message.from_user.first_name};
+        LAST_NAME: {message.from_user.last_name};
+        USERNAME: {message.from_user.username}."""
